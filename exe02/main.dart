c0a39a26289745c1032d411117b3cb02d17dba6a
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-19 04:04:01 -0300

    Enviando funcionalidade X

---
+++ exe02/main.dart
@@ -0,0 +1,5 @@
+void main() {
+  for (var j = 0; j < 10; j++) {
+    print("Olá Mundo Curso de git");
+  }
+}
--- texto.dart
+++ texto.dart
@@ -1,3 +1,4 @@
 void main() {
   print("Olá mundo !!!");
+  print("Seja Bem Vindo !!!");
 }
