64744b1baef099824d62e606cb93aa35a494035e
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-21 16:33:26 -0300

    21-04-2023

--- exe03/main.dart
+++ exe03/main.dart
@@ -1,8 +1,10 @@
-void main() {}
+void main() {
+  closures();
+}
 
 void closures() {
   Function funcao(double j) => (double g) => j * g;
   var funcaoV = funcao(50.0);
   print(funcaoV);
-  print("")
+  print("Resultado ${funcaoV(10.0)}");
 }
